02e5cfbbb2f39693173505814e964f0e5ec9dc12
Alexander Zagaynov <zalex80@gmail.com>
2015-04-24 02:57:53 +0300

    N+1 fix for translations

--- app/models/fallacy.rb
+++ app/models/fallacy.rb
@@ -1,6 +1,7 @@
 class Fallacy < ActiveRecord::Base
   extend FriendlyId; friendly_id :slug
   translates :name, :description, fallbacks_for_empty_translations: true
+  default_scope { includes(:translations) }
   has_many :statements, inverse_of: :fallacy
 
   def cache_key
--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,4 +1,5 @@
 class Statement < ActiveRecord::Base
   translates :description
+  default_scope { includes(:translations) }
   belongs_to :fallacy, inverse_of: :statements
 end
