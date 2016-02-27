6307c2ddb88afaf9c59ed43daaab26abf51c8c89
Alexander Zagaynov <zalex80@gmail.com>
2016-02-28 06:47:31 +0700

    layout restyling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
 
+  def each_locale current_first: false, &block
+    locales = current_first ? [I18n.locale] : []
+    locales = locales + (I18n.available_locales - locales)
+    locales.each { |locale| concat(capture locale, locale == I18n.locale, &block) }
+  end
+
   def navbar_link destination
     name = t destination, scope: :navigation
-    options = { class: :active } if controller_path == destination.to_s
+    options = { class: :current } if controller_path == destination.to_s
     content_tag(:li, options) { link_to name, controller: "/#{destination}" }
   end
 
